ec30b471537a4161076e3d5a6399586363986e12
霍春燃 <254191664@qq.com>
2021-03-10 17:44:34 +0800

    test

---
+++ springbeanlive/src/main/java/com/hcr/springbeanlive/SpringbeanliveApplication.java
@@ -0,0 +1,13 @@
+package com.hcr.springbeanlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeanliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbeanliveApplication.class, args);
+    }
+
+}
---
+++ springbeanlive/src/test/java/com/hcr/springbeanlive/SpringbeanliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcr.springbeanlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbeanliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
